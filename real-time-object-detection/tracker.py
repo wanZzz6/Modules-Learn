52ebc7cdaaca2f7def8a593da27ae8d46b806206
wanZzz <1343837706@qq.com>
2019-07-30 08:11:04 +0800

    目标跟踪

---
+++ real-time-object-detection/tracker.py
@@ -0,0 +1,111 @@
+'''
+https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/
+
+
+'''
+
+import cv2
+import sys
+
+trackerTypes = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+
+
+def createTrackerByName(trackerType):
+    # Create a tracker based on tracker name
+    if trackerType == trackerTypes[0]:
+        tracker = cv2.TrackerBoosting_create()
+    elif trackerType == trackerTypes[1]:
+        tracker = cv2.TrackerMIL_create()
+    elif trackerType == trackerTypes[2]:
+        tracker = cv2.TrackerKCF_create()
+    elif trackerType == trackerTypes[3]:
+        tracker = cv2.TrackerTLD_create()
+    elif trackerType == trackerTypes[4]:
+        tracker = cv2.TrackerMedianFlow_create()
+    elif trackerType == trackerTypes[5]:
+        tracker = cv2.TrackerGOTURN_create()
+    elif trackerType == trackerTypes[6]:
+        tracker = cv2.TrackerMOSSE_create()
+    elif trackerType == trackerTypes[7]:
+        tracker = cv2.TrackerCSRT_create()
+    else:
+        tracker = None
+        print('Incorrect tracker name')
+        print('Available trackers are:')
+        for t in trackerTypes:
+            print(t)
+
+    return tracker
+
+
+# Set up tracker.
+# Instead of MIL, you can also use
+
+bboxes = []
+
+
+def init_tracker(frame, trackerType='CSRT'):
+    # Create MultiTracker object
+    multiTracker = cv2.MultiTracker_create()
+
+    # 使用第一帧和边界框初始化单个对象跟踪器，
+    # 该边界框指示我们想要跟踪的对象的位置
+    # MultiTracker将此信息传递给它内部包装的单个目标跟踪器。
+    for bbox in bboxes:
+        multiTracker.add(createTrackerByName(trackerType), frame, bbox)
+
+    return multiTracker
+
+
+if __name__ == '__main__':
+    # Read video
+    video = cv2.VideoCapture("chaplin.mp4")
+
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    # Define an initial bounding box
+    bboxes = [(287, 23, 86, 320)]
+
+    # Uncomment the line below to select a different bounding box
+    # bbox = cv2.selectROI(frame, False)
+
+    # Initialize tracker with first frame and bounding box
+    multiTracker = init_tracker(frame)
+
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        # Update tracker
+        success, boxes = multiTracker.update(frame)
+        print(success)
+
+        if success:
+            # draw tracked objects
+            for i, newbox in enumerate(boxes):
+                p1 = (int(newbox[0]), int(newbox[1]))
+                p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
+                cv2.rectangle(frame, p1, p2, (0, 255, 0), 2)
+
+        else:
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+        # Display result
+        cv2.imshow("Tracking", frame)
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
