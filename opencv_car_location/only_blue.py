443343ce166929a056d3ccb463e2a34d8b613c9f
wanzheng <1343837706@qq.com>
2019-08-29 10:13:02 +0800

    设置中间过程的结果的显示与等待

--- opencv_car_location/only_blue.py
+++ opencv_car_location/only_blue.py
@@ -8,9 +8,11 @@ maxPlateRatio = 6  # 车牌最大比例
 # 定义蓝底车牌的hsv颜色区间
 # lower_blue = np.array([100, 40, 50])
 # higher_blue = np.array([140, 255, 255])
-lower_blue = np.array([100, 40, 50])
-higher_blue = np.array([140, 255, 255])
+lower_blue = np.array([70, 0, 50])
+higher_blue = np.array([140, 255, 220])
 
+SHOW = True
+WAIT_KEY = False
 
 # 找到符合车牌形状的矩形
 def findPlateNumberRegion(img):
@@ -26,7 +28,7 @@ def findPlateNumberRegion(img):
         # 计算轮廓面积
         area = cv2.contourArea(cnt)
         # 面积小的忽略
-        if area < 1000:
+        if area < 1500:
             continue
         # 转换成对应的矩形（最小）
         rect = cv2.minAreaRect(cnt)
@@ -54,7 +56,7 @@ def findPlateNumberRegion(img):
 def getSatifyestBox(list_rate):
     if len(list_rate) == 0:
         print('未匹配到区域')
-        exit(0)
+        exit(input('按任意键退出'))
     for index, key in enumerate(list_rate):
         list_rate[index] = abs(key - 3)
     print(list_rate)
@@ -78,47 +80,61 @@ def getxyRate(cnt):
 def location(frame):
     # 转换成hsv模式图片
     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # cv2.imshow("hsv_img", hsv_img)
-    # cv2.waitKey(0)
+    if SHOW:
+        cv2.imshow("hsv_img", hsv_img)
+    if WAIT_KEY:
+        cv2.waitKey(0)
 
     # 找到hsv图片下的所有符合蓝底颜色区间的像素点，转换成二值化图像
     mask = cv2.inRange(hsv_img, lower_blue, higher_blue)
     res = cv2.bitwise_and(img, img, mask=mask)
-    # cv2.imshow("res", res)
-    # cv2.waitKey(0)
+    if SHOW:
+        cv2.imshow("res", res)
+    if WAIT_KEY:
+        cv2.waitKey(0)
 
     # 灰度化
     gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("gray", gray)
-    # cv2.waitKey(0)
+    if SHOW:
+        cv2.imshow("gray", gray)
+    if WAIT_KEY:
+        cv2.waitKey(0)
 
     # 高斯模糊：车牌识别中利用高斯模糊将图片平滑化，去除干扰的噪声对后续图像处理的影响
     gaussian = cv2.GaussianBlur(gray, (3, 3), 0, 0, cv2.BORDER_DEFAULT)
-    # cv2.imshow("gaussian", gaussian)
-    # cv2.waitKey(0)sian)
-    #     # cv2.waitKey(0)
+    if SHOW:
+        cv2.imshow("gaussian", gaussian)
+    if WAIT_KEY:
+        cv2.waitKey(0)
 
     # sobel算子：车牌定位的核心算法，水平方向上的边缘检测，检测出车牌区域
     sobel = cv2.convertScaleAbs(cv2.Sobel(gaussian, cv2.CV_16S, 1, 0, ksize=3))
-    # cv2.imshow("sobel", sobel)
-    # cv2.waitKey(0)
+    if SHOW:
+        cv2.imshow("sobel", sobel)
+    if WAIT_KEY:
+        cv2.waitKey(0)
 
     # 进一步对图像进行处理，强化目标区域，弱化背景。
     ret, binary = cv2.threshold(sobel, 150, 255, cv2.THRESH_BINARY)
-    cv2.imshow("binary", binary)
-    # cv2.waitKey(0)
-
+    if SHOW:
+        cv2.imshow("binary", binary)
+    if WAIT_KEY:
+        cv2.waitKey(0)
     # 进行闭操作，闭操作可以将目标区域连成一个整体，便于后续轮廓的提取
     element = cv2.getStructuringElement(cv2.MORPH_RECT, (17, 5))
     closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, element)
+
     # 进行开操作，去除细小噪点
     # eroded = cv2.erode(closed, None, iterations=1)
     # dilation = cv2.dilate(eroded, None, iterations=1)
-    cv2.imshow("closed", closed)
-    # cv2.waitKey(0)
+
+    if SHOW:
+        cv2.imshow("closed", closed)
+    if WAIT_KEY:
+        cv2.waitKey(0)
 
     # 查找并筛选符合条件的矩形区域
-    region = findPlateNumberRegion(closed)
+    region = findPlateNumberRegion(sobel)
     print('目标区域', region)
     cv2.drawContours(img, [region], 0, (0, 255, 0), 2)
     cv2.imshow("img", img)
@@ -126,6 +142,9 @@ def location(frame):
 
 
 if __name__ == '__main__':
+    SHOW = True
+    WAIT_KEY = False
     file = "car_pic/2.jpg"
+    file = r"C:\Users\13438\Desktop\bb.png"
     img = cv2.imread(file)
     location(img)
